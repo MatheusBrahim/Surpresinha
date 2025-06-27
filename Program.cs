7f61a1db69a8bb3994abf158a20bfccc1c8d8dec
MatheusBrahim <matheusbrahim.face@gmail.com>
2025-06-27 18:27:35 -0300

    Resolvido

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int jogadas;
+ 
+Console.Clear();
+Console.WriteLine("--- Surpresinha ---\n");
+ 
+Console.Write("Quantas jogadas? ");
+ if (!int.TryParse(Console.ReadLine(), out  jogadas) || jogadas <= 0)
+   {
+      Console.WriteLine("Número inválido.");
+            return;
+   }
+ 
+        var random = new Random();
+ 
+for (int i = 0; i < jogadas; i++)
+{
+    var numeros = new HashSet<int>();
+ 
+    while (numeros.Count < 6)
+    numeros.Add(random.Next(1, 61));
+ 
+    var resultado = numeros.OrderBy(n => n);
+    Console.WriteLine(string.Join(" - ", resultado));
+}
